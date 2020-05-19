6100df6cd1f8a72203dea827888e7085848ea1ae
Biringa Chidera <biringachidera@gmail.com>
2020-05-18 21:09:09 -0400

    Update -- Implemented scroll behaviour

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
-var TxtType = function(el, toRotate, period) {
+(function() {
+    scrollTo();
+})();
+
+function scrollTo() {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+            link.onclick = scrollAnchors;
+        }
+    }
+}
+
+// function scrollTo() {
+//     const links = document.querySelectorAll('.scroll');
+//     links.forEach(each => (each.onclick = scrollAnchors));
+// }
+
+function scrollAnchors(e, respond = null) {
+    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+    e.preventDefault();
+    var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+    const targetAnchor = document.querySelector(targetID);
+    if (!targetAnchor) return;
+    const originalTop = distanceToTop(targetAnchor);
+    window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+    const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+            targetAnchor.tabIndex = '-1';
+            targetAnchor.focus();
+            window.history.pushState('', '', targetID);
+            clearInterval(checkIfDone);
+        }
+    }, 100);
+}
+
+
+let TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
@@ -9,8 +48,8 @@ var TxtType = function(el, toRotate, period) {
     };
 
     TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+        let i = this.loopNum % this.toRotate.length;
+        let fullTxt = this.toRotate[i];
 
         if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
@@ -20,8 +59,8 @@ var TxtType = function(el, toRotate, period) {
 
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        var that = this;
-        var delta = 200 - Math.random() * 100;
+        let that = this;
+        let delta = 200 - Math.random() * 100;
 
         if (this.isDeleting) { delta /= 2; }
 
@@ -40,23 +79,17 @@ var TxtType = function(el, toRotate, period) {
     };
 
     window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
+        let elements = document.getElementsByClassName('typewrite');
+        for (let i=0; i<elements.length; i++) {
+            let toRotate = elements[i].getAttribute('data-type');
+            let period = elements[i].getAttribute('data-period');
             if (toRotate) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        // INJECT CSS
-        var css = document.createElement("style");
+
+        let css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
-
-new TypeIt("#simpleUsage", {
-  strings: "This is a simple string.",
-  speed: 50,
-  waitUntilVisible: true
-}).go();
